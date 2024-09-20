f2b2ae15283aa2520cd18886b670f54a80cd8a77
senja <senja.vuoristo@aalto.fi>
2024-09-20 12:53:45 +0300

    Muokkasin Eliksen musitiinpanoja.

--- Eliaksen musitiinpanot.py
+++ Eliaksen musitiinpanot.py
@@ -1,10 +1,10 @@
-#* jälkee luku millä välillä se randomoi
+# * jälkee luku millä välillä se randomoi
 # RAND() randomoi
-#floor pyörisrtää alas ceil ylös
-select RAND() * 1000 AS price, name, description FROM airport WHERE code='Helsinki'
+# floor pyörisrtää alas ceil ylös
+# select RAND() * 1000 AS price, name, description FROM airport WHERE code='Helsinki'
 
-#randomoi valitut kenttien määrä (order by rand())
-#Select name, iso_country
-#from airport
-#order by rand()
-#limit 10;
+# randomoi valitut kenttien määrä (order by rand())
+# Select name, iso_country
+# from airport
+# order by rand()
+# limit 10;
