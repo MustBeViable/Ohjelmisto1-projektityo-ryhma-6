b4bdc32266241207a946e70eea887dfce4227735
senja <senja.vuoristo@aalto.fi>
2024-09-20 22:22:59 +0300

    Pari parannusta, laajennettiin ohjetta.

--- Game/actions.py
+++ Game/actions.py
@@ -2,14 +2,15 @@
 # as string variables in game_texts. They are all imported to this file.
 from Game.game_texts import *
 
-# The function asks for a command from the user with the prompt given to it as a parameter.
+
+# The function asks for a command from the user with the prompt given to the function as a parameter.
 # The prompt can be for example "Anna komento."
 # The function executes the action connected to the command and returns ???
 # After executing the commmand, the function repeats itself asking for a new commmand. (Should it?)
 
 def pick_action(prompt):
-    action = input(prompt).upper()
-    if action == ask_help:
+    command = input(prompt).lower()
+    if command == ask_help:
         print(give_help)
         pass
     else:
@@ -18,7 +19,7 @@ def pick_action(prompt):
     return
 
 
-# Testi
+# Test
 test_prompt = give_commmand
 pick_action(test_prompt)
 
--- Game/game_texts.py
+++ Game/game_texts.py
@@ -1,5 +1,5 @@
 # All the commands that the user can give
-ask_help = "OHJE"
+ask_help = "ohje"
 
 
 # All game texts
@@ -9,6 +9,16 @@ give_commmand = 'Anna komento: '
 not_command = ('Tuo ei  ollut komento. '
                'Komennolla "ohje" näet kaikki mahdolliset komennot. ')
 
-give_help = (f'"ohje": Näyttää tämän ohjeen.\n'
-             f'"komento": Tekee jotain\n'
-             f'"toinen komento": Tekee jotain muuta')
+give_help = (f'\n'
+             f'PELIN OHJE\n'
+             f'Kirjoita haluamasi komento ja paina enter. Alta näet mahdolliset komennot.\n'
+             f'\n'
+             f'KOMENNOT\n'
+             f'{ask_help}: Näyttää tämän ohjeen.\n'
+             f'komento: Tekee jotain\n'
+             f'toinen komento: Tekee jotain muuta\n'
+             f'\n'
+             f'PELIN TAVOITE\n'
+             f'Pelin tavoite on kerätä mahdollisimman paljon makkaroita.\n'
+             f'Erilaisista makkaroista saa enemmän pisteitä.\n'
+             f'Peli päättyy, kun rahasi loppuvat tai käytät lopetuskomentoa.\n')
