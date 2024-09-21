f2e7302f56a565370f81025d87c754c651258acf
Elias Rinne <elias1rinne@gmail.com>
2024-09-21 18:46:39 +0300

    vielä kesken. Haku hakee 20 random kenttää ja ne tulee list of dictionaries. En vielä keksinyt miten voin tallettaa tietoja jotenki pelaajan valittaviksi

---
+++ airport_selection_function.py
@@ -0,0 +1,39 @@
+import mysql.connector
+from geopy import distance
+
+yhteys = mysql.connector.connect(
+         host='localhost',
+         port=3306,
+         database='flight_game',
+         user='eliasellu',
+         password='Koira123',
+         autocommit=True,
+         collation="utf8mb4_general_ci"
+         )
+#Tää funktio hakee 20 random kenttää ja saa sen nimen, maan ja leveys/pituuspiirit geopyy varten
+def airportselection():
+    sql = (f" Select name, iso_country, latitude_deg, longitude_deg "
+           f" from airport "
+           f" where type = 'large_airport'"
+           f" order by rand()"
+           f" limit 20 ")
+    #kursori = yhteys.cursor(dictionary=true), muuttaa tuplen sijasta dictionaryks. Eli lista, jonka sisällä dictionary
+    #kantsii käyttä jos useampi select (vaikka select name, ident, id...) ja muuttamalla select arvoja ei muuta printtiä
+    #näi voi callaa avaimilla mm result['name']
+    kursori = yhteys.cursor(dictionary=True)
+    kursori.execute(sql)
+    result = kursori.fetchall()
+    for i in result:
+        print(i)
+    return result
+#tää funktio määrittää hinnan etäisyyksien perusteella
+def distance():
+    #tähän sit jotai geopyjutskaa
+    #mm. dist = distance.distance(CURRENT_AIRPORT, AIRPORT_FROM_LIST).km (jos haluu kilsoina)
+airportselection()
+#ICAO1 = input("Insert first ICAO code: ")
+#ICAO2 = input("Insert second ICAO code: ")
+#airport1 = cordinate(ICAO1)
+#airport2 = cordinate(ICAO2)
+#dist = distance.distance(airport1, airport2).km
+#print(f"Lentokenttien etäisyys on {dist:.2f} km.")
