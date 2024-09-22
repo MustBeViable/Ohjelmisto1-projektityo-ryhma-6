24953834e40af07d772d22e209d7a02ae2aeb2e6
senja <senja.vuoristo@aalto.fi>
2024-09-22 21:15:32 +0300

    Refaktoroi komennon yhdistäminen toimintoon

--- Game/actions.py
+++ Game/actions.py
@@ -1,25 +1,37 @@
 # All the commands that the user can give and all the texts that the user can see are stored
-# as string variables in game_texts. They are all imported to this file.
-from Game.game_texts import *
+# as string variables in game_texts.py.
+from Game.game_texts import give_help_str, help_command, not_command_str, give_commmand_str, fly_command
 
+# Prints the game instruction and asks for a new command.
+def give_help():
+    print(give_help_str)
+    pick_action()
+
+# Prints that the given command does not exist and asks for a new one.
+def faulty_command(command):
+    print(f'"{command}"{not_command_str}')
+    pick_action()
+
+# Joku tällanen tulee tähän mut katotaan sit sit myöhemmin tää oli vaan esimerkki
+def show_airports():
+    pass
 
 # The function asks for a command from the user with the prompt given to the function as a parameter.
-# The prompt can be for example "Anna komento."
-# The function executes the action connected to the command and returns ???
-# After executing the commmand, the function repeats itself asking for a new commmand. (Should it?)
-
-def pick_action(prompt):
-    command = input(prompt).lower()
-    if command == ask_help:
-        print(give_help)
-        pass
+# The prompt can be for example "Olet Nummelan lentokentällä. Mitä haluat tehdä?"
+# The function executes the action connected to the command and returns ??? pitäs varmaan palauttaa jotain
+def pick_action():
+    command = input("").lower()
+    if command == help_command:
+        give_help()
+    elif command == fly_command:
+        show_airports()
     else:
-        print(not_command)
-    pick_action(give_commmand)
+        faulty_command(command)
     return
 
 
-# Test
-test_prompt = give_commmand
-pick_action(test_prompt)
 
+# Test
+test_prompt = give_commmand_str
+print("Peli on käynnissä. Aloita antamalla ensimmäinen komento.")
+pick_action()
