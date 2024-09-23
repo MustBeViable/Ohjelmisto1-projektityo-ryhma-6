bf7a8d845d6555c68451a6f157773c3ae328d746
tuukka <tuukkauu@metropolia.fi>
2024-09-23 17:55:13 +0300

    Robber function works

--- Game/garbage_can.py
+++ Game/garbage_can.py
@@ -1,47 +1,33 @@
 
-# import saken mustamakkarafunktio yhdistettävä tähän funktioon
-# import rahamäärä
-
-# 10# mahis finnair_personnel, 10# mahis kolovastaavalle, 10# mahis rosvolle, 70# mahis muulle
+# 10% mahis finnair_personnel, 10% mahis kolovastaavalle, 10% mahis rosvolle, 70% mahis muulle
 
 import random
 
-from Game.actions import pick_action
+from Game.player_profile import own_makkaras, own_money
 
 
-#Checkng garbages
+#Checkng garbages, saken mustamakkarafunktio
 def garbage_can ():
-    pick_action()
-    player_money = 100
+    own_makkaras()
+    player_money = own_money
 
     # the possibilities of different outcomes
     outcome  = random.choices(['nothing', 'robber', 'hole_in_charge', 'finnair_personnel'], weights = [70, 10, 10, 10], k=1)[0]
 
 
-def robber(player_money):
+def robber(own_money):
+    player_money = own_money
     if player_money > 0:
         stolen_money = random.randint(40, 60) / 100
         new_money = player_money * stolen_money
         return new_money
 
+
 def hole_in_charge():
     pass
 
+def finnair_personnel():
+    pass
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-player_money = 500
-player_money = robber(player_money)
-print(player_money)
+#Testing that the robber function works
+print(robber(own_money))
