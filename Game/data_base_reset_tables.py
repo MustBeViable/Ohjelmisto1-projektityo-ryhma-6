bbaf664cdd49c4b832febce51e25e5c6a7c504ad
Elias Rinne <elias1rinne@gmail.com>
2024-09-26 19:44:24 +0300

    Funktio joka poistaa tableja listan mukaa järjestyksessä. Eli pystyy vähän resetoimaa automaattisesti tietokantaa, jos meni jokin pielee. Siel on test_list niminen lista, johon lisäämällä saa poistamaa myös sen taulun

---
+++ Game/data_base_reset_tables.py
@@ -0,0 +1,11 @@
+from game_texts import yhteys
+
+def table_remove(table):
+    sql = "DROP TABLE {table} "
+    kursori = yhteys.cursor()
+    kursori.execute(sql.format(table=table))
+    return
+#lisää listaa tablen nimi ku lisäät uuden tablen tietokantaa
+test_list = ["makkara", "makkara_located", "playthrough"]
+for i in test_list:
+    table_remove(table=i)
