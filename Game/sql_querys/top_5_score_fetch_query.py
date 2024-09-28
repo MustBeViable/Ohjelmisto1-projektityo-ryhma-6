a72f059766e2f1e3c9eb5e9a81c5fee2e00e590d
Elias Rinne <elias1rinne@gmail.com>
2024-09-28 08:19:50 +0300

    korjasin ettei kaadu jos alle 5 scoree top vitoseen

--- Game/sql_querys/top_5_score_fetch_query.py
+++ Game/sql_querys/top_5_score_fetch_query.py
@@ -9,8 +9,12 @@ def top_5_score_fetch_query():
     result = kursori.fetchall()
     result_sorted = sorted(result, key=lambda x: x['score'], reverse=True)
     print(f"Top 5 lista: ")
-    for i in range(5):
-         print(f"{result_sorted[i]['screen_name']}: {result_sorted[i]['score']:6.0f}")
+    if len(result_sorted) < 5:
+        for i in range(len(result_sorted)):
+            print(f"{result_sorted[i]['screen_name']}: {result_sorted[i]['score']:6.0f}")
+    else:
+        for i in range(5):
+             print(f"{result_sorted[i]['screen_name']}: {result_sorted[i]['score']:6.0f}")
     return
 
 top_5_score_fetch_query()
