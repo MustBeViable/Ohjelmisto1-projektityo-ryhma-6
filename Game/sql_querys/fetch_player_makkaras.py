4584aa2377a989a9e06f9c5827452272311a0427
Elias Rinne <elias1rinne@gmail.com>
2024-09-28 09:26:02 +0300

    Lisäsin kommentin mitä se tekee ja lisäsin toisen samanlaisen funktion missä voi tarkentaa screen_nameen myös. En muista mikä oli tarkotus. FUnktiot yhä palauttaa listan makkaroiden id:stä

--- Game/sql_querys/fetch_player_makkaras.py
+++ Game/sql_querys/fetch_player_makkaras.py
@@ -1,7 +1,8 @@
 from Game.game_texts import yhteys
 
-
-def fetch_player_makkaras(id):
+#Luo listan pelaajan makkatoista joita pelaajalla on. Parametri on pelaajan uniikki id, pitäs varmaa tarkentaa
+# screen_name kanssa?
+def fetch_player_makkaras1(id):
     sql = (f" SELECT makkara_id"
            f" FROM makkara_reached"
            f" where game_id = '{id}'")
@@ -12,3 +13,23 @@ def fetch_player_makkaras(id):
     for i in range(len(result)):
         player_makkaras.append(result[i][0])
     return player_makkaras
+
+def fetch_player_makkaras2(id, screen_name):
+    sql = (f" SELECT makkara_id"
+           f" FROM makkara_reached, playthrough"
+           f" where game_id = playthrough.id"
+           f" AND game_id = '{id}'"
+           f" AND screen_name = '{screen_name}'")
+    print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    result = kursori.fetchall()
+    player_makkaras = []
+    for i in range(len(result)):
+        player_makkaras.append(result[i][0])
+    return player_makkaras
+
+'''
+koloherra = 'koloherra'
+print(fetch_player_makkaras2(1,koloherra))
+'''
