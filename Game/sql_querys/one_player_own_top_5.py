7e1def7accc3801259eae80d68ca6a312ce42005
Elias Rinne <elias1rinne@gmail.com>
2024-09-28 08:18:10 +0300

    korjasin ettei kaadu jos alle 5 scoree top vitoseen

--- Game/sql_querys/one_player_own_top_5.py
+++ Game/sql_querys/one_player_own_top_5.py
@@ -22,12 +22,15 @@ def unique_player_top_5_score_fetch_query(screen_name):
     result = kursori.fetchall()
     result_sorted = sorted(result, key=lambda x: x['score'], reverse=True)
     print(f"Pelaajan {screen_name} top 5 lista")
-    for i in range(5):
+    if len(result_sorted) < 5:
+        for i in range(len(result_sorted)):
+            print(f"{result_sorted[i]['screen_name']}: {result_sorted[i]['score']:6.0f}")
+    else:
+        for i in range(5):
          print(f"{result_sorted[i]['screen_name']}: {result_sorted[i]['score']:6.0f}")
     return
 
-'''
-unique_player_top_5_score_fetch_query('koloherra')
+
+unique_player_top_5_score_fetch_query('testi2')
 print("")
-all_unique_player_score_fetch_query('koloherra')
-'''
+all_unique_player_score_fetch_query('testi1')
