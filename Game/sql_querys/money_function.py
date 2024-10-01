ca319d227a492862a18939d51c97f44fcb6aacfb
sakari Honkavaara <Sakari.honkavaara@metropolia.fi>
2024-10-01 14:06:41 +0300

    sql_querys kansioon money_func

--- Game/sql_querys/money_function.py
+++ Game/sql_querys/money_function.py
@@ -1,18 +1,23 @@
+#Tää puuttu kokonaan nii ei tee sql hakua
+from Game.game_texts import yhteys
 
-def add_money(amount):
-    sql = (f'UPDATE game SET money = {amount} WHERE id = {game_id};')
+#lisäsin toiset parametrit molempiin eli game_id, johon syötetään sen pelikerran id. Päivittää siis oikeita tietoja.
+# Sama alempaa funktioon. Muutin myös tablen oikeaksi. SQL komento ei tarvitse ";" suorittaakseen oikein.
+def add_money(amount, game_id):
+    sql = (f"UPDATE playthrough SET money = {amount} WHERE id = '{game_id}'")
     kursori = yhteys.cursor()
     kursori.execute(sql)
     return
-
-def use_money(spent):
-    sql = (f"SELECT money FROM game WHERE id = {game_id};")
+#lisäsin tänne sen game_id parametrin jotta päivitetään aina oikeaa tietoa. Se tuo myös funktion sisällä add_money
+# funktioon saman tiedon ja mulla toimi oikein toi nyt.
+def use_money( game_id):
+    sql = (f"SELECT money FROM playthrough WHERE id = '{game_id}'")
     kursori = yhteys.cursor(dictionary=True)
     kursori.execute(sql)
     result = kursori.fetchall()
-    new_money = result[0]['money']
-    new_money -= spent
-    add_money(new_money)
-    print(result)
-    return result
-#monke
+    player_money = result[0]['money']
+    print(player_money)
+
+    return
+
+use_money(1)
