f4df37e2edfd81d0f590fa34e4708a5f2d04e4ee
MustBeViable <126315806+MustBeViable@users.noreply.github.com>
2024-10-01 16:22:34 +0300

    Elias (#46)
    * Nyt toimii tarkistuskin
    
    * Nyt toimii ja testaa onko jo tablet olemassa ennen ku luo.
    
    * Funktio joka poistaa tableja listan mukaa järjestyksessä. Eli pystyy vähän resetoimaa automaattisesti tietokantaa, jos meni jokin pielee. Siel on test_list niminen lista, johon lisäämällä saa poistamaa myös sen taulun
    
    * oli typo, jonka korjasin.
    
    * Päivitetty toimimaan oikein
    
    * Päivitin että money on int eikä varchar
    
    * Lisäsin sovittuja tauluja ja laitoin sen luomaa tietoja heti ensi alkuun.
    
    * muokattu yhteys olin tietoja normiksi
    
    * Tarvittavat listat tietokannan luontiin
    
    * Laitoin funktion testi promptit kommenteiksi
    
    * toinen dictionary
    
    * Hakee kaikki scoret, järjestää ne suurimmasta pienimpää ja tulostaa 5 tulosta semi nätisti
    
    * Päivitin nimen oikeeksi
    
    * 2 funktiota jossa toisessa se hakee scoren parametrinaan pelaajan id. Toinen päivittää scoren jonka parametreinä on uusi score ja pelaajan id.
    
    * 2 funktiota jossa toisessa se hakee pelaajan sijainnin parametrinään pelaajan id. Toinen funktio päivittää locationin parametreinaan pelaajan id ja new_location, joka tulee olemaan ICAO koodi, joka saadaan airport_selection_functionista
    
    * poistin turhan filun
    
    * FUnktio hakee syötetyllä makkaran id:llä scoren (funktion parametriin id)
    
    * Mustamakkara funktille ehdotus.
    
    Funktio1:
    
    Hakee pelaajan id:n mukaa sen hetkisen mustamakkaratilanteen ja returnaa sen eli voi printtaa print(fetch_mustamakkara(id))
    
    funktio2:
    
    Päivittää pelaajan id perusteella (parametri 1) uuden mustamakkara arvon tietokantaa (parametri 2)
    
    * vein tiedostot oikeesen kansioon
    
    * sakelle ehdotukset korjaukseen ja selitykset mitö tein ja miks
    
    * Uniikin pelaajan top-5 ja kaikki score fetch funktiot
    
    * Lisäsin printin joka kertoo mitä tapahtuu
    
    * Hakee pelaajan game_id tiedolla sen pelaajan makkarat ja luo siitä listan, jonka returnaa.
    
    * korjasin ettei kaadu jos alle 5 scoree top vitoseen
    
    * korjasin ettei kaadu jos alle 5 scoree top vitoseen
    
    * Lisäsin kommentin mitä se tekee ja lisäsin toisen samanlaisen funktion missä voi tarkentaa screen_nameen myös. En muista mikä oli tarkotus. FUnktiot yhä palauttaa listan makkaroiden id:stä
    
    * Lisäsin kommentin mitä se tekee ja lisäsin toisen samanlaisen funktion missä voi tarkentaa screen_nameen myös. En muista mikä oli tarkotus. FUnktiot yhä palauttaa listan makkaroiden id:stä
    
    * Päivitin sql queryt
    
    * Päivitin sql queryt
    
    * Päivitin sql queryt taulukon mukaseksi
    
    * postin turhan

--- Game/sql_querys/fetch_player_makkaras.py
+++ Game/sql_querys/fetch_player_makkaras.py
@@ -2,25 +2,10 @@ from Game.game_texts import yhteys
 
 #Luo listan pelaajan makkatoista joita pelaajalla on. Parametri on pelaajan uniikki id, pitäs varmaa tarkentaa
 # screen_name kanssa?
-def fetch_player_makkaras1(id):
+def fetch_player_makkaras(id):
     sql = (f" SELECT makkara_id"
            f" FROM makkara_reached"
            f" where playthrough_id = '{id}'")
-    kursori = yhteys.cursor()
-    kursori.execute(sql)
-    result = kursori.fetchall()
-    player_makkaras = []
-    for i in range(len(result)):
-        player_makkaras.append(result[i][0])
-    return player_makkaras
-
-def fetch_player_makkaras2(id, screen_name):
-    sql = (f" SELECT makkara_id"
-           f" FROM makkara_reached, playthrough"
-           f" where playthrough_id = playthrough.id"
-           f" AND playthrough_id = '{id}'"
-           f" AND screen_name = '{screen_name}'")
-    print(sql)
     kursori = yhteys.cursor()
     kursori.execute(sql)
     result = kursori.fetchall()
