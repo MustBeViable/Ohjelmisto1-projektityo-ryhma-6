4e6965654541bef2300a831e537305a02d8528f3
senja <senja.vuoristo@aalto.fi>
2024-10-04 15:22:10 +0300

    Lisättiin komentoja.

--- Game/section.py
+++ Game/section.py
@@ -7,43 +7,40 @@ from Game.taxfree import yes_no_taxfree
 
 class Section:
     """Sections of the game. Each section happens max once in one airport."""
-    def __init__(self, question, action, approved_answer, other_answer, art):
+    def __init__(self, question, action, approved_answer, other_answer, art, function_to_number, condition_minimum, condition_explanation):
         self.question = str(question)
         self.action = action
         self.approved_answer = approved_answer
         self.other_answer = other_answer
         self.art = art
-
-    def check_condition(self, parameter_int):
-        """Checks if the player fills the condition that has been set for the section. Returns boolean."""
-        return True
-
-class SectionWithNumberCondition(Section):
-    def __init__(self, question, action, approved_answer, other_answer, art, function_to_number, condition_minimum, condition_explanation):
-        super().__init__(question, action, approved_answer, other_answer, art)
-        self.condition_function = function_to_number
+        self.function_to_number = function_to_number
         self.condition_minimum = condition_minimum
         self.condition_explanation = condition_explanation
 
     def check_condition(self, parameter_int):
-        if self.condition_function(parameter_int) >= self.condition_minimum:
+        if self.function_to_number(parameter_int) >= self.condition_minimum:
             return True
-
 
 
 garbage_can_section = Section(question=garbage_can_question,
                               action=garbage_can,
                               approved_answer=[yes],
                               other_answer=[no],
-                              art=ascii_carbage_can)
+                              art=ascii_carbage_can,
+                              function_to_number=fetch_player_money,
+                              condition_minimum=1,
+                              condition_explanation="Rahasi loppuivat!")
 
 tax_free_section = Section(question=tax_free_question,
                            action=yes_no_taxfree,
                            approved_answer=[yes],
                            other_answer=[no],
-                           art="")
+                           art="",
+                           function_to_number=fetch_player_money,
+                           condition_minimum=1,
+                           condition_explanation="Rahasi loppuivat!")
 
-flight_section = SectionWithNumberCondition(question=fligh_question,
+flight_section = Section(question=fligh_question,
                                             action=airportselection,
                                             approved_answer=[approve],
                                             other_answer=[],
