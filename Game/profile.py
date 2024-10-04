1cb5d614f46909c4b4e22b8b01e3ac4f49a40f40
tuukka <tuukkauu@metropolia.fi>
2024-10-04 15:15:04 +0300

    Profiili toimii, tarvitsee vain lisätä peliin.

--- Game/profile.py
+++ Game/profile.py
@@ -1,20 +1,15 @@
-# player makkaras eri komennolla lisäksi, jos haluaa
-# player location
-# player money
-# player score
-# player top score ?
-# player stolen makkaras
 from Game.sql_querys.fetch_player_makkaras import fetch_player_makkaras, fetch_player_stolen_makkaras
 from Game.sql_querys.money_function import fetch_player_money
 from Game.sql_querys.player_location_fetch_and_update_querys import fetch_player_location, fetch_player_location_name
+from Game.sql_querys.score_fetch_and_score_update_querys import player_score_fetch
 
 
 def profile(game_id):
     """This funktion shows player's profile"""
-    print(f"Sijaintisi on tällä hetkellä: {fetch_player_location_name(game_id)}", {fetch_player_location(game_id)})
-    print(f"Sinulla on rahaa {fetch_player_money(game_id)} euroa.")
+    print(f"Sijaintisi on tällä hetkellä lentokentällä: {fetch_player_location_name(game_id)}, ICAO: {fetch_player_location(game_id)}")
+    print(f"Sinulla on rahaa {fetch_player_money(game_id)} euroa ja scoresi on {player_score_fetch(game_id)}")
     #print(f"Sinun scoresi on {score_fetch(game_id)}")
-    show_makkarat = input("Jos haluat nähdä keräämäsi makkarat, anna komento 'makkarani'.: ")
+    show_makkarat = input("Jos haluat nähdä, paljonko sinulla on makkaroita, anna komento 'makkarani'.: ")
     if show_makkarat == 'makkarani':
         print(f"Sinulla on yhteensä makkaroita: {len(fetch_player_makkaras(game_id))} kpl.")
         """
@@ -25,7 +20,7 @@ def profile(game_id):
         show_makkarat_dictionary = {f"{fetch_player_location_name(game_id)}", f"{fetch_player_makkaras(game_id)}"}
         #print(show_makkarat)
         """
-profile(2)
+
 
 
 
