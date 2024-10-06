252c36fe0d61f4c429293d5aa6bcf7c377dc8412
senja <senja.vuoristo@aalto.fi>
2024-10-06 21:38:54 +0300

    Tulosta top 5 listat.

--- Game/profile.py
+++ Game/profile.py
@@ -8,12 +8,12 @@ from Game.sql_querys.score_fetch_and_score_update_querys import player_score_fet
 def show_profile(game_id):
     """This funktion shows player's profile"""
     print(f"Sijaintisi on tällä hetkellä lentokentällä: {fetch_player_location_name(game_id)}, ICAO: {fetch_player_location(game_id)}")
-    print(f"Sinulla on rahaa {fetch_player_money(game_id)} euroa ja scoresi on {player_score_fetch(game_id)}")
+    print(f"Sinulla on rahaa {fetch_player_money(game_id)} euroa ja pisteitä {player_score_fetch(game_id)}.")
     #print(f"Sinun scoresi on {score_fetch(game_id)}")
-    show_makkarat = input("Jos haluat nähdä, paljonko sinulla on makkaroita, anna komento 'makkarani'.: ")
-    if show_makkarat == 'makkarani':
-        print(f"Sinulla on yhteensä makkaroita: {player_makkaras_amount(game_id)} kpl.")
-        """
+    #show_makkarat = input("Jos haluat nähdä, paljonko sinulla on makkaroita, anna komento 'makkarani'.: ")
+    print(f"Sinulla on yhteensä makkaroita: {player_makkaras_amount(game_id)} kpl.")
+    #if show_makkarat == 'makkarani':
+    """
         print(f"Kolovastaava viemät makkarat: {fetch_player_stolen_makkaras(game_id)}")
         print(f"Sinulla on {fetch_player_makkaras(game_id)}")
 
